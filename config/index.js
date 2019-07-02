c76835f465e9e480e4c11e896603dd4c976d632d
Garrosh <969417321@qq.com>
2019-07-02 10:02:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
     // https server
     https:true,
     // Various Dev Server settings
-    host: '127.0.0.11', // can be overwritten by process.env.HOST
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
     port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
