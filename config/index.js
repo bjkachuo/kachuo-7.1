a7a7b82e794897d5a4b606dd376dd2680cef7aa9
gy2986589 <815436974@qq.com>
2019-07-04 16:25:46 +0800

    修改图标2

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
     // https server
     https:true,
     // Various Dev Server settings
-    host: '192.168.1.122', // can be overwritten by process.env.HOST
+    host: '192.168.0.194', // can be overwritten by process.env.HOST
     port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
