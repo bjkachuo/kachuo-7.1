1bc6cc2bfd9b0d6de70f5448535e7ff9d864a22b
lisi <969417321@qq.com>
2019-09-02 14:32:56 +0800

    9.2

---
+++ src/components/pages/kachuoincome/YaoYueChuangZuo/listShow.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "listShow"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/pages/tabindex/KachuoTabIncome/agreement.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "agreement"
+    }
+</script>
+
+<style scoped>
+
+</style>
