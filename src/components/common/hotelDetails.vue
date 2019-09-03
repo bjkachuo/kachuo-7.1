3e5617be393eccf617e70822b20b914555259ea5
gy2986589 <815436974@qq.com>
2019-09-03 13:38:26 +0800

    0903y

--- src/components/common/hotelDetails.vue
+++ src/components/common/hotelDetails.vue
@@ -13,7 +13,7 @@
         v-for="(item, index) in this.storeDetails.image"
         @click="show(index)"
         :key="index"
-      /> -->
+      />-->
       <!-- <div v-transfer-dom>
         <previewer
           :list="this.storeDetails.image"
@@ -21,7 +21,7 @@
           :options="options"
           @on-index-change="logIndexChange"
         ></previewer>
-      </div> -->
+      </div>-->
     </div>
     <div class="hotelDetails-title">
       <div style="height: 51px;">
@@ -89,6 +89,102 @@
         <span>地图/导航</span>
       </div>
     </div>
+    <div class="inTime-wrap">
+      <div class="inTime-wrap-left">
+        <div class="house-img-wrap">
+          <img src="../../assets/images/lou.png" alt />
+        </div>
+        <p>入住时间-退房时间</p>
+      </div>
+      <div class="inTime-wrap-right">
+        <p>共1晚</p>
+        <x-icon type="ios-arrow-right" size="20" class="vux-x-icon-two"></x-icon>
+      </div>
+    </div>
+    <div class="house-list-wrap">
+      <div class="house-list-title">
+        <h3>在线订房</h3>
+        <div class="list-right">
+          <p>筛选</p>
+        </div>
+      </div>
+      <div class="house-list">
+        <div class="house-list-left">
+          <div class="house-list-left-imgwrap">
+            <img src alt />
+          </div>
+          <div class="house-list-left-text">
+            <div class="text-one">
+              <p>自主大床房</p>
+            </div>
+            <div class="text-two">
+              <p>不含早 大床 有窗</p>
+            </div>
+            <div class="text-three">
+              <p>不可取消</p>
+            </div>
+            <div class="text-four">
+              <p>￥102</p>
+            </div>
+          </div>
+        </div>
+        <div class="house-list-right">
+          <p>预订</p>
+        </div>
+      </div>
+      <div class="house-list">
+        <div class="house-list-left">
+          <div class="house-list-left-imgwrap">
+            <img src alt />
+          </div>
+          <div class="house-list-left-text">
+            <div class="text-one">
+              <p>自主大床房</p>
+            </div>
+            <div class="text-two">
+              <p>不含早 大床 有窗</p>
+            </div>
+            <div class="text-three">
+              <p>不可取消</p>
+            </div>
+            <div class="text-four">
+              <p>￥102</p>
+            </div>
+          </div>
+        </div>
+        <div class="house-list-right">
+          <p>预订</p>
+        </div>
+      </div>
+      <div class="house-list">
+        <div class="house-list-left">
+          <div class="house-list-left-imgwrap">
+            <img src alt />
+          </div>
+          <div class="house-list-left-text">
+            <div class="text-one">
+              <p>自主大床房</p>
+            </div>
+            <div class="text-two">
+              <p>不含早 大床 有窗</p>
+            </div>
+            <div class="text-three">
+              <p>不可取消</p>
+            </div>
+            <div class="text-four">
+              <p>￥102</p>
+            </div>
+          </div>
+        </div>
+        <div class="house-list-right">
+          <p>预订</p>
+        </div>
+      </div>
+      <div class="Surplus">
+        <p>查看剩余房型</p>
+        <x-icon type="ios-arrow-down" size="20" class="vux-x-icon-four"></x-icon>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -150,10 +246,10 @@ export default {
     goBack() {
       this.$router.go(-1);
     },
-    //切换图
-    logIndexChange(arg) {
-      console.log(arg);
-    },
+    // //切换图
+    // logIndexChange(arg) {
+    //   console.log(arg);
+    // },
     show(index) {
       this.$refs.previewer.show(index);
     }
@@ -379,19 +475,174 @@ export default {
   display: block;
   float: left;
   color: #666666ff;
-  font-size: 16px;
+  font-size: 15px;
   line-height: 55px;
   margin-left: 4%;
+  width: 65%;
+  height: 55px;
 }
 .address-wrap .inner-wrap {
   float: right;
+  margin: 15px 3% 0 0;
 }
 .address-img-wrap {
   float: left;
+  width: 15px;
+  height: 15px;
 }
 .inner-wrap span {
   display: block;
   float: right;
+  font-size: 16px;
+  color: #3976ffff;
+}
+.inTime-wrap {
+  width: 100%;
+  height: 55px;
+  border-radius: 8px;
+  margin-bottom: 10px;
+  background: #ffffffff;
+}
+.inTime-wrap-left {
+  height: 55px;
+  width: 60%;
+  float: left;
+  /* background: fuchsia; */
+}
+.house-img-wrap {
+  width: 15px;
+  height: 15px;
+  margin: 13px 4% 0 7%;
+  float: left;
+}
+.house-img-wrap img {
+  width: 100%;
+  background: none;
+}
+.inTime-wrap-left p {
+  display: block;
+  float: left;
+  font-size: 16px;
+  color: #222222ff;
+  margin-top: 13px;
+}
+.inTime-wrap-right {
+  height: 55px;
+  width: 62px;
+  float: right;
+
+  color: #3976ffff;
+  font-size: 16px;
+}
+.inTime-wrap-right p {
+  float: left;
+  display: block;
+  margin-top: 12px;
+}
+.house-list-wrap {
+  width: 100%;
+  background: #ffffff;
+  border-radius: 8px;
+}
+.house-list-title {
+  width: 100%;
+  height: 55px;
+}
+.house-list-title h3 {
+  color: #222222ff;
+  font-size: 18px;
+  font-weight: 800;
+  float: left;
+  margin: 11px 0 0 4%;
+  display: block;
+}
+.list-right {
+  width: 65px;
+  height: 55px;
+  float: right;
+}
+.list-right p {
+  color: #222222ff;
+  font-size: 14px;
+  display: block;
+  float: left;
+  margin-top: 18px;
+  margin-left: 17%;
+}
+.house-list {
+  width: 92%;
+  height: 75px;
+  margin: 0 auto 30px;
+  /* background: navy; */
+}
+.house-list-left {
+  width: 50%;
+  height: 75px;
+  float: left;
+  width: 70%; /* background: darkorange; */
+}
+.house-list-left-imgwrap {
+  width: 75px;
+  height: 75px;
+  float: left;
+  margin-right: 13px;
+}
+.house-list-left-imgwrap img {
+  width: 100%;
+}
+.house-list-left-text {
+  height: 75px;
+  width: 110px;
+  float: left;
+}
+.text-one p {
+  font-size: 14px;
+  color: #222222ff;
+  font-weight: bold;
+}
+.text-two p {
+  color: #666666ff;
+  font-size: 12px;
+  font-weight: normal;
+}
+.text-three p {
+  color: #666666ff;
+  font-size: 12px;
+  font-weight: normal;
+}
+.text-four p {
+  color: #ff3939ff;
+  font-size: 12px;
+}
+
+.house-list-right {
+  width: 70px;
+  height: 30px;
+  float: right;
+  background: #3976ffff;
+  margin-top: 8px;
+  border-radius: 8px;
+}
+.house-list-right p {
+  font-size: 16px;
+  color: #ffffffff;
+  font-weight: 800;
+  text-align: center;
+  line-height: 30px;
+}
+.Surplus {
+  height: 55px;
+  width: 100%;
+  border-radius: 8px;
+  margin-bottom: 20px;
+  background: #ffffffff;
+  padding-top: 20px;
+}
+.Surplus p {
+  display: block;
+  float: left;
+  overflow: hidden;
+  margin-left: 4%;
 }
 </style>
 <style lang="less" scoped>
@@ -402,5 +653,16 @@ export default {
 .vux-x-icon-two {
   fill: #222222ff;
   margin: 15px 0 0 0;
+}
+.vux-x-icon-three {
+  fill: #222222ff;
+  display: block;
+  float: left;
+  margin: 17px 0 0 0px;
+}
+.vux-x-icon-four {
+  fill: #222222ff;
+  display: block;
+  margin-top: 2px;
 }
 </style>
