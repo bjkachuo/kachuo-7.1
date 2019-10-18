048de7accc3e4255a51eb530c9697ba4601cb82d
gy2986589 <815436974@qq.com>
2019-10-18 14:57:52 +0800

    1018gy1

--- src/components/pages/kachuouser/UserIntegral.vue
+++ src/components/pages/kachuouser/UserIntegral.vue
@@ -6,13 +6,40 @@
       :showRightMore="TitleObjData.showRightMore"
     ></Header>
     <div class="user-interal-content-wrap" :style="consty">
-      <div class="user-interal-desc">
+      <!-- <div class="user-interal-desc">
         <p class="tip">当前积分</p>
         <p class="num">
           <span style="position:relative;top:-20px">{{userInfo.credit1}}</span>
         </p>
+      </div>-->
+      <!-- <CellDivider :cellList="cellListTools" class="user-interal-wrap-tools-wrap"></CellDivider> -->
+      <div class="card">
+        <div class="txt">
+          <p>当前积分</p>
+        </div>
+        <div class="num">
+          <p>{{userInfo.credit1}}</p>
+        </div>
+        <div class="lineOne"></div>
+        <div class="bottom">
+          <div class="b-one">
+            <p>积分提现</p>
+          </div>
+          <div class="line-two"></div>
+          <div class="b-two">
+            <p @click="goChong">积分充值</p>
+          </div>
+        </div>
       </div>
-      <CellDivider :cellList="cellListTools" class="user-interal-wrap-tools-wrap"></CellDivider>
+      <div class="bot">
+        <div class="bot-one">
+          <p>积分规则</p>
+        </div>
+        <div class="bot-two"></div>
+        <div class="bot-thr">
+          <p>联系客服</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -66,11 +93,13 @@ export default {
   beforeMount() {},
 
   mounted() {
-          console.log(this.$store.state.userLoginInfo);
+    console.log(this.$store.state.userLoginInfo);
   },
 
   methods: {
-
+    goChong() {
+      this.$router.push("/Recharge");
+    }
   },
 
   watch: {}
@@ -80,12 +109,14 @@ export default {
 .user-interal-wrap {
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  overflow: hidden scroll;
 }
 .user-interal-content-wrap {
   width: 100%;
   margin-top: 50px;
   background: #f9f9f9;
+  overflow: hidden;
+  position: relative;
 }
 .user-interal-desc {
   width: 100%;
@@ -115,5 +146,96 @@ export default {
   justify-content: center;
   align-items: center;
   font-size: 30px;
+}
+.card {
+  width: 92%;
+  height: 200px;
+  color: #fff;
+  background: linear-gradient(
+    90deg,
+    rgba(31, 100, 255, 1),
+    rgba(39, 141, 255, 1)
+  );
+  box-shadow: 0px 10px 25px 0px rgba(57, 118, 255, 0.5);
+  border-radius: 8px;
+  margin: 15px auto 0;
+  overflow: hidden;
+}
+.txt {
+  margin: 15px 0 34px 15px;
+}
+.num {
+  margin-bottom: 39px;
+}
+.num p {
+  text-align: center;
+}
+.lineOne {
+  height: 1px;
+  width: 92%;
+  background: #4da0ff;
+  margin: 0 auto;
+}
+.bottom {
+  /* width: 92px; */
+  height: 50px;
+  margin: 0 auto;
+}
+.b-one {
+  float: left;
+  height: 50px;
+  width: 49.5%;
+}
+.b-one p {
+  text-align: center;
+  line-height: 40px;
+  font-size: 16px;
+  color: #80bfff;
+}
+.line-two {
+  float: left;
+  width: 1px;
+  height: 30px;
+  background: #4da0ff;
+  margin-top: 6px;
+}
+.b-two {
+  float: left;
+  height: 50px;
+  width: 49.5%;
+}
+.b-two p {
+  text-align: center;
+  line-height: 40px;
+  font-size: 16px;
+  color: #ffffff;
+}
+.bot {
+  width: 131px;
+  margin: 0 auto;
+  position: absolute;
+  bottom: 4px;
+  left: 32%;
+}
+.bot-one {
+  float: left;
+}
+.bot-one p {
+  font-size: 14px;
+  color: #666666;
+}
+.bot-two {
+  float: left;
+  height: 13px;
+  width: 1px;
+  background: #666666;
+  margin: 5px 5px 0;
+}
+.bot-thr {
+  float: left;
+}
+.bot-thr p {
+  font-size: 14px;
+  color: #666666;
 }
 </style>
