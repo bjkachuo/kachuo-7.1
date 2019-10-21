c83cd48ad3f63b3039fa5e8c1193170cc9e789ef
gy2986589 <815436974@qq.com>
2019-10-21 14:58:01 +0800

    1021gy1

--- src/components/pages/kachuouser/mallAdmin/Cartment.vue
+++ src/components/pages/kachuouser/mallAdmin/Cartment.vue
@@ -1,10 +1,16 @@
 <template>
-<div class="wrap">
-    <x-header :left-options="{showBack: showLeftBack,preventGoBack:true}" @on-click-back='back' slot="header" style="width: 100%;position: absolute;left: 0;top: 0;z-index: 100;font-size: 20px;">{{titleContent}}
-    <div slot="right">
-      <div class="x-txt" v-if="toggle" @click="onEdit()">{{rText}}</div>
-      <div class="x-txt" v-if="!toggle" @click="onOver()">{{rText}}</div>
-    </div>
+  <div class="wrap">
+    <x-header
+      :left-options="{showBack: showLeftBack,preventGoBack:true}"
+      @on-click-back="back"
+      slot="header"
+      style="width: 100%;position: absolute;left: 0;top: 0;z-index: 100;font-size: 20px;"
+    >
+      {{titleContent}}
+      <div slot="right">
+        <div class="x-txt" v-if="toggle" @click="onEdit()">{{rText}}</div>
+        <div class="x-txt" v-if="!toggle" @click="onOver()">{{rText}}</div>
+      </div>
     </x-header>
     <div class="normal-content" :style="conHei">
       <div class="cart-panel" v-for="(item,index) in this.ListOne" :key="index">
@@ -16,7 +22,7 @@
           <template slot="icon">
             <check-icon :value.sync="check2"></check-icon>
             <div class="gar-photo">
-              <img src="./ddpic.png" alt="">
+              <img src="./ddpic.png" alt />
             </div>
           </template>
           <template slot="after-title">
@@ -25,8 +31,11 @@
                 <div class="gar-title">{{item.goods_sx.title}}</div>
               </div>
               <div class="gar-foot">
-                <div class="gar-price">￥<span>{{item.marketprice}}</span></div>
-                <inline-x-number :min="0" width="30px" :value="item.total" ></inline-x-number>
+                <div class="gar-price">
+                  ￥
+                  <span>{{item.marketprice}}</span>
+                </div>
+                <inline-x-number :min="0" width="30px" :value="item.total"></inline-x-number>
               </div>
             </div>
           </template>
@@ -35,52 +44,75 @@
     </div>
     <div class="cart-tabbar">
       <label class="check-box">
-        <check-icon :value.sync="check1"></check-icon><span>全选</span>
+        <div @click="test">
+          <check-icon :value.sync="check1"></check-icon>
+        </div>
+        <span>全选</span>
       </label>
-      <div class="total-price" v-if="toggle">合计:<span>￥1500.00</span></div>
+      <div class="total-price" v-if="toggle">
+        合计:
+        <span>{{this.price}}</span>
+      </div>
       <div class="button" v-if="toggle">去结算</div>
       <div class="button del-button" v-if="!toggle">删除</div>
     </div>
-
   </div>
 </template>
 
 <script>
 import { ShopList, goodsBucketRecomm, goodsBucketSubmit } from "@/servers/api";
-import { XHeader,Tabbar, TabbarItem ,Cell,XButton,InlineXNumber,CheckIcon} from 'vux'
+import {
+  XHeader,
+  Tabbar,
+  TabbarItem,
+  Cell,
+  XButton,
+  InlineXNumber,
+  CheckIcon
+} from "vux";
 export default {
   name: "",
   props: [""],
   data() {
     return {
-      toggle:true,
+      toggle: true,
       titleContent: "购物车",
       showLeftBack: true,
       rText: "编辑",
-      check1:true,
-      check2:true,
-      ListOne:[],
-      num:""
+      check1: true,
+      check2: true,
+      ListOne: [],
+      num: "",
+      price: 0
     };
   },
-  methods:{
-    onEdit(){
-      this.rText = "完成"
-      this.toggle = !this.toggle
+  methods: {
+    test() {
+      this.check1 != this.check1
+      console.log(this.check1)
+      // if(this.check1 = true){
+      //   console.log("11111")
+      // }else{
+      //   console.log("22222")
+      // }
+},
+    onEdit() {
+      this.rText = "完成";
+      this.toggle = !this.toggle;
     },
-    back(){
-     this.$router.go(-1)
+    back() {
+      this.$router.go(-1);
     },
-    onOver(){
-      this.rText = "编辑"
-      this.toggle = !this.toggle
+    onOver() {
+      this.rText = "编辑";
+      this.toggle = !this.toggle;
     },
     getDataList() {
-      ShopList({page: 1})
+      ShopList({ page: 1 })
         .then(res => {
           console.log(res);
-          this.ListOne = res.data.result
-          console.log("购物车列表",this.ListOne)
+          this.ListOne = res.data.result;
+          console.log("购物车列表", this.ListOne);
           // if (res.result === 1) {
           //   let obj = res.data.result;
           //   for (let i in obj) {
@@ -94,36 +126,42 @@ export default {
         })
         .catch(err => {
           console.log(err);
-        })
+        });
     }
   },
   components: {
-    XHeader, XButton, Tabbar,TabbarItem ,Cell,InlineXNumber,CheckIcon
+    XHeader,
+    XButton,
+    Tabbar,
+    TabbarItem,
+    Cell,
+    InlineXNumber,
+    CheckIcon
   },
-  mounted(){
-    this.$nextTick(()=>{
+  mounted() {
+    this.$nextTick(() => {
       console.log(1);
-      this.getDataList()
-    })
+      this.getDataList();
+    });
   },
   computed: {
     conHei() {
       return { height: document.documentElement.clientHeight - 90 + "px" };
     }
-  },
+  }
 };
 </script>
 <style lang='css' scoped>
-.normal-content{
+.normal-content {
   width: 100%;
-  background: #F5F5F5;
+  background: #f5f5f5;
   overflow: hidden;
   overflow-y: scroll;
   padding: 15px 0;
   box-sizing: border-box;
 }
-.cart-tabbar{
-  background-color: #FFFFFF;
+.cart-tabbar {
+  background-color: #ffffff;
   position: absolute;
   padding: 10px 15px;
   left: 0;
@@ -131,76 +169,76 @@ export default {
   bottom: 48px;
   overflow: hidden;
 }
-.cart-tabbar .button{
+.cart-tabbar .button {
   width: 100px;
   height: 35px;
   line-height: 35px;
-  background-color: #3976FF;
+  background-color: #3976ff;
   border-radius: 35px;
-  color: #FFFFFF;
+  color: #ffffff;
   text-align: center;
   float: right;
 }
-.cart-tabbar .del-button{
-  color: #3976FF;
+.cart-tabbar .del-button {
+  color: #3976ff;
   background-color: transparent;
-  box-shadow: 0 0 0 1px #3976FF;
+  box-shadow: 0 0 0 1px #3976ff;
 }
-.cart-tabbar .check-box{
+.cart-tabbar .check-box {
   float: left;
 }
-.check-box{
+.check-box {
   padding: 5px 0;
   font-size: 16px;
 }
-.check-box /deep/ .weui-icon{
+.check-box /deep/ .weui-icon {
   margin-top: -3px;
 }
-.check-box /deep/ *{
+.check-box /deep/ * {
   display: inline-block;
   vertical-align: middle;
 }
 .check-box /deep/ .vux-check-icon > .weui-icon-success:before,
-.check-box /deep/ .vux-check-icon > .weui-icon-success-circle:before{
-  color: #3976FF;
+.check-box /deep/ .vux-check-icon > .weui-icon-success-circle:before {
+  color: #3976ff;
 }
-.cart-tabbar .total-price{
+.cart-tabbar .total-price {
   float: left;
-  line-height:35px;
+  line-height: 35px;
   font-size: 14px;
   margin-left: 20px;
 }
-.cart-tabbar .total-price span{
+.cart-tabbar .total-price span {
   font-weight: bold;
   font-size: 16px;
 }
 
-.x-txt{
+.x-txt {
   color: #222;
   font-size: 16px;
 }
-.cart-panel{
-  background:rgba(255,255,255,1);
-  box-shadow:0px 10px 15px 0px rgba(0,101,255,0.05);
-  border-radius:8px;
+.cart-panel {
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0px 10px 15px 0px rgba(0, 101, 255, 0.05);
+  border-radius: 8px;
   overflow: hidden;
   margin-bottom: 15px;
   padding: 15px 0;
 }
-.cart-panel .weui-cell{
-  padding:8px 15px;
+.cart-panel .weui-cell {
+  padding: 8px 15px;
 }
-.gar-photo{
+.gar-photo {
   width: 90px;
   height: 90px;
   margin-right: 15px;
 }
-.gar-photo img{
+.gar-photo img {
   width: 90px;
   height: 90px;
   border-radius: 4px;
 }
-.gar-body{
+.gar-body {
   height: 90px;
   line-height: 1.4;
   display: -webkit-box;
@@ -209,9 +247,9 @@ export default {
   -webkit-box-pack: justify;
   -webkit-justify-content: space-between;
   justify-content: space-between;
-  flex-direction:column
+  flex-direction: column;
 }
-.gar-foot{
+.gar-foot {
   position: relative;
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
@@ -225,51 +263,51 @@ export default {
   -webkit-align-items: center;
   align-items: center;
 }
-.cart-panel /deep/ .weui-cell__hd{
+.cart-panel /deep/ .weui-cell__hd {
   position: relative;
   padding-left: 30px;
 }
-.cart-panel /deep/ .weui-cell__hd .vux-check-icon{
+.cart-panel /deep/ .weui-cell__hd .vux-check-icon {
   position: absolute;
   left: -5px;
   top: 50%;
   margin-top: -11px;
 }
 .cart-panel /deep/ .vux-check-icon > .weui-icon-success:before,
-.cart-panel /deep/ .vux-check-icon > .weui-icon-success-circle:before{
-  color: #3976FF;
+.cart-panel /deep/ .vux-check-icon > .weui-icon-success-circle:before {
+  color: #3976ff;
 }
-.cart-panel /deep/ .vux-number-selector{
+.cart-panel /deep/ .vux-number-selector {
   width: 25px;
   height: 25px;
   padding: 0;
   text-align: center;
   line-height: 25px;
-  background-color: #EBEEF5;
+  background-color: #ebeef5;
   border-radius: 3px;
   margin: 0;
 }
 .cart-panel /deep/ .vux-number-selector svg {
-    fill: #999;
+  fill: #999;
 }
-.cart-panel /deep/ .vux-number-input{
+.cart-panel /deep/ .vux-number-input {
   height: 25px;
   line-height: 25px;
   border: 0;
 }
-.cart-panel /deep/ .vux-inline-x-number{
+.cart-panel /deep/ .vux-inline-x-number {
   padding: 1px;
-  border: 1px solid #EBEEF5;
+  border: 1px solid #ebeef5;
   border-radius: 3px;
 }
-.weui-cell:before{
+.weui-cell:before {
   display: none;
 }
-.cart-header{
+.cart-header {
   padding: 0 10px;
   margin-bottom: 10px;
 }
-.cart-header .shop-name{
+.cart-header .shop-name {
   font-size: 16px;
   font-weight: bold;
   position: relative;
@@ -279,13 +317,12 @@ export default {
   background: url(./rt_arrow2.png) right center no-repeat;
   background-size: 12px 12px;
 }
-.gar-price{
-  color: #FF3939;
+.gar-price {
+  color: #ff3939;
   font-size: 12px;
 }
-.gar-price span{
+.gar-price span {
   font-size: 15px;
   font-weight: bold;
 }
-
 </style>
